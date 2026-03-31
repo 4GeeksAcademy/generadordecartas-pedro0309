eb377cd39d7f73758ab3ce0e3132c37efb1a9e53
pedro0309 <pedro00030009@gmail.com>
2026-03-31 21:54:59 +0000

    generador de cartas listo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  // Números
+  let numbers = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+
+  document.getElementById("number").innerHTML = numbers[randomNumber];
+
+  // Iconos
+  let icons = ["♦","♥","♠","♣"];
+  let randomIcon = Math.floor(Math.random() * icons.length);
+  let selectedIcon = icons[randomIcon];
+
+  document.getElementById("icon1").innerHTML = selectedIcon;
+  document.getElementById("icon2").innerHTML = selectedIcon;
+
+  
+  if (selectedIcon === "♦" || selectedIcon === "♥") {
+    document.getElementById("card").style.color = "red";
+  } else {
+    document.getElementById("card").style.color = "black";
+  }
+
 };
